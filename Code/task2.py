5496e10c11733bd2bf65d8a746f6eaffc974afc9
Gleb <skryabin.gleb.d@gmail.com>
2020-12-25 21:02:35 +0300

    small ref

---
+++ Code/task2.py
@@ -0,0 +1,24 @@
+from SLE import SLE
+from Gauss import gauss
+from DataSet import DataSet
+from linalg import det, inverse, print_matrix, cond, norm
+
+
+
+
+
+def main():
+    print("\n" * 10)
+    dataset = DataSet()
+    sle = dataset.sle[0]
+
+
+
+
+
+    
+
+
+
+
+main() 
