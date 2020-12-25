333704a9538f13b1d5e7f45574c9ae7b32848bc8
Gleb <skryabin.gleb.d@gmail.com>
2020-12-25 23:07:03 +0300

    нормальный метод релаксации

--- Code/task2.py
+++ Code/task2.py
@@ -5,44 +5,50 @@ from DataSet import DataSet
 
 
 def main():
+    print("\n" * 30)
     dataset = DataSet()
     sle = dataset.testsle
     sle.show()
 
-    w = 1
-    eps = 1e-3
-    done = False
-    iteration = 0
+    for eps in [0.01, 0.000001]:
+        print("eps = {}".format(eps))
+        for w in [0.5, 1, 1.1, 1.12, 1.2]:        
+            done = False
+            iteration = 0
+            x = [0 for i in range(sle.N)]
+            xnext = [0 for i in range(sle.N)]
+            while (not done):
+                norm = 0
+                x = [a for a in xnext]
+                xnext = [0 for i in range(sle.N)]
+                for i in range(sle.N):
+                    delta = sle.B[i]
+                    for j in range(0, i - 1):
+                        delta -= sle.A[i][j] * xnext[j]
+                    for j in range(i - 1, sle.N):
+                        delta -= sle.A[i][j] * x[j]
+                    delta *= w / sle.A[i][i]
+                    norm += delta ** 2
+                    xnext[i] = x[i] + delta
 
-    x = [0 for i in range(sle.N)]
-    xnext = [0 for i in range(sle.N)]
-    while (not done):
-        norm = 0
-        x = [a for a in xnext]
-        xnext = [0 for i in range(sle.N)]
+                norm **= (1/2)
+                done = norm < eps
+                iteration += 1
+                #print("{0:2d}:  xk+1 = [".format(iteration), end="")
+                #for i in range(sle.N):
+                #    print("{0:.2f}".format(xnext[i]), end = ", ")
+                #print(end="]\n")
 
-        for i in range(sle.N):
-            delta = sle.B[i]
-            for j in range(0, i - 1):
-                delta -= sle.A[i][j] * xnext[j]
-            for j in range(i - 1, sle.N):
-                delta -= sle.A[i][j] * x[j]
-
-            delta *= w / sle.A[i][i]
-            norm += delta ** 2
-            xnext[i] = x[i] + delta
-
-        norm **= (1/2)
-        done = norm < eps
-        iteration += 1
-        print("{0:2d}:  xk+1 = [".format(iteration), end="")
-        for i in range(sle.N):
-            print("{0:.2f}".format(xnext[i]), end = ", ")
-        print(end="]\n")
-
-    print("Gauss: ", gauss(sle))
+            print("    w = {0:4f}, iters = {1:3d}".format(w, iteration))
 
 
 
 
 main() 
+
+
+
+
+'''3 -1 1 3
+1 1 1 5
+4 -1 4 5'''
