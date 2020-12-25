1159de4bd196324b260e8d1fea6fa65d162891f9
Gleb <skryabin.gleb.d@gmail.com>
2020-12-25 22:53:55 +0300

    qwevkjbv

--- Code/task2.py
+++ Code/task2.py
@@ -1,50 +1,46 @@
 from SLE import SLE
+from Gauss import gauss
 from DataSet import DataSet
 
 
 
 def main():
-    print("\n" * 25)
     dataset = DataSet()
     sle = dataset.testsle
     sle.show()
 
     w = 1
-    it = 1
-    eps = 1e-4
+    eps = 1e-3
     done = False
+    iteration = 0
 
     x = [0 for i in range(sle.N)]
     xnext = [0 for i in range(sle.N)]
     while (not done):
-        x = [xnext[i] for i in range(sle.N)]
+        norm = 0
+        x = [a for a in xnext]
         xnext = [0 for i in range(sle.N)]
-
-        
 
         for i in range(sle.N):
             delta = sle.B[i]
-            for j in range(0, i):
+            for j in range(0, i - 1):
                 delta -= sle.A[i][j] * xnext[j]
             for j in range(i - 1, sle.N):
                 delta -= sle.A[i][j] * x[j]
-            delta *= w / sle.A[i][i]
 
+            delta *= w / sle.A[i][i]
+            norm += delta ** 2
             xnext[i] = x[i] + delta
 
-
-
-
-
-        v = sum([(xnext[i] - x[i]) ** 2 for i in range(sle.N)])
-        done = v < eps
-        it+=1
-        print("iteration = {0} norm = {1:.6f}\n\txk+1 = [".format(it, v), end = "")
+        norm **= (1/2)
+        done = norm < eps
+        iteration += 1
+        print("{0:2d}:  xk+1 = [".format(iteration), end="")
         for i in range(sle.N):
             print("{0:.2f}".format(xnext[i]), end = ", ")
         print(end="]\n")
 
-
+    print("Gauss: ", gauss(sle))
 
 
 
